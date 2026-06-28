6644ce8c6a38088b4c682c491090e23b770891f6
RomanB <moldwebs@gmail.com>
2026-06-28 13:39:37 +0300

    x

--- start.sh
+++ start.sh
@@ -4,15 +4,16 @@ COMFYUI_DIR=/workspace/ComfyUI
 VENV_PYTHON="$COMFYUI_DIR/.venv/bin/python"
 echo "------------Comfyui V1.1 - Starting"
 # ── Launch Jupyter Lab ───────────────────────────────────────────
-# echo "  → Starting Jupyter Lab on port 8888..."
-# jupyter lab \
-#     --ip=0.0.0.0 \
-#     --port=8888 \
-#     --no-browser \
-#     --allow-root \
-#     --NotebookApp.token='' \
-#     --NotebookApp.password='' \
-#     > /workspace/jupyter.log 2>&1 &
+echo "  → Starting Jupyter Lab on port 8888..."
+jupyter lab \
+    --allow-root \
+    --notebook-dir=/ \
+    --ip=0.0.0.0 \
+    --port=8888 \
+    --no-browser \
+    --ServerApp.token='12345' \
+    --ServerApp.allow_origin='*' \
+    > /workspace/jupyter.log 2>&1 &
 # ── Launch ComfyUI ───────────────────────────────────────────────
 echo "  → Launching ComfyUI on port 8188..."
 echo ""
